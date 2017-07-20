21cb274c46d6e42a7b4b25e0bb225637e67632f0
Peter Hedinger <peter@xmos.com>
2017-07-20 07:15:17 +0000

    Add commentary and delays to launch

--- launch_avs.sh
+++ launch_avs.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+echo "Starting Companion Service"
 cd /home/pi/Desktop/alexa-avs-sample-app/samples/companionService
 lxterminal -e "npm start"
 
+sleep 2
+echo "Starting Java Client"
 cd /home/pi/Desktop/alexa-avs-sample-app/samples/javaclient
 lxterminal -e "mvn exec:exec"
 
+sleep 10
+echo "Starting Sensory Keyword Agent"
 cd /home/pi/Desktop/alexa-avs-sample-app/samples/wakeWordAgent/src
 lxterminal -e "./wakeWordAgent -e sensory"
