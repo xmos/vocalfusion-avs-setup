d2a47cecc2f9c0699c2bfb112950d332efb892a4
Brennan Magee <brennan@xmos.com>
2017-09-28 10:57:37 +0100

    Make comments about sensory model clearer

--- scripts/avs-MAIN.sh
+++ scripts/avs-MAIN.sh
@@ -4,7 +4,7 @@ export SCRIPTS_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 SENSORY_MODEL_HASH_1=a8befe708af1aa80c32bce5219312a4ec439a0b0
 SENSORY_MODEL_HASH_2=ddbc9040e24ed06aafe402017fa640b86d3520b3
 SENSORY_MODEL_HASH_3=43b5cb246cb8422a8f39ae92d3e372dc19b98243
-# Select Sensory model to use
+# Select Sensory version to use (default is 2)
 export SENSORY_MODEL_HASH=$SENSORY_MODEL_HASH_2
 
 pushd .
