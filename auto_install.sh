17d81f283e39d125748b9076714de06b971ba5ef
BrennanGit <brennan@xmos.com>
2017-09-29 09:58:13 +0100

    Update scripts path to correct location

--- auto_install.sh
+++ auto_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SCRIPTS_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+export SCRIPTS_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/scripts
 SENSORY_MODEL_HASH_1=a8befe708af1aa80c32bce5219312a4ec439a0b0
 SENSORY_MODEL_HASH_2=ddbc9040e24ed06aafe402017fa640b86d3520b3
 SENSORY_MODEL_HASH_3=43b5cb246cb8422a8f39ae92d3e372dc19b98243
