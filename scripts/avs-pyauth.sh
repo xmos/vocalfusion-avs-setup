acc269370cf118b08eadde065043ca7fed2ba05c
Brennan Magee <brennan@xmos.com>
2017-10-03 10:48:10 +0100

    Update python authorisation piping

--- scripts/avs-pyauth.sh
+++ scripts/avs-pyauth.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd $HOME/BUILD
 sudo apt-get install -y python-flask python-requests | sed "s/^/[py packages] /"
-python AuthServer/AuthServer.py > /dev/null &
+python AuthServer/AuthServer.py | grep -v '400' &
 chromium-browser http://localhost:3000 > /dev/null 2>&1
 wait
 cp $HOME/BUILD/Integration/AlexaClientSDKConfig.json $SCRIPTS_DIR/
