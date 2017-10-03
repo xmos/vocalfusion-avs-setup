dc3b51e03e29c5a599c8c8af6563d0b1af7eb2ac
Brennan Magee <brennan@xmos.com>
2017-10-03 09:56:58 +0100

    Open web browser automatically for authentication

--- scripts/avs-pyauth.sh
+++ scripts/avs-pyauth.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 cd $HOME/BUILD
 sudo apt-get install -y python-flask python-requests | sed "s/^/[py packages] /"
-echo Open localhost:3000 and enter amz dev credentials
-python AuthServer/AuthServer.py
+python AuthServer/AuthServer.py &
+ServerPID=$! &
+chromium-browser http://localhost:3000
+wait $ServerPID
 cp $HOME/BUILD/Integration/AlexaClientSDKConfig.json $SCRIPTS_DIR/
