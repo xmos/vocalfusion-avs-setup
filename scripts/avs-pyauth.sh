6bdd92266b30ad6a31429883bfaa92922af51361
Brennan Magee <brennan@xmos.com>
2017-10-03 15:04:51 +0100

    Kill any process using tcp:3000

--- scripts/avs-pyauth.sh
+++ scripts/avs-pyauth.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cd $HOME/BUILD
 sudo apt-get install -y python-flask python-requests | sed "s/^/[py packages] /"
+sudo fuser -k -TERM -n tcp 3000
 python AuthServer/AuthServer.py | grep -v '400' &
 chromium-browser http://localhost:3000 > /dev/null 2>&1
 wait
