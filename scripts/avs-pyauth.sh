82fc59bfa0cc94a630b202808a875837008ebfdb
Brennan Magee <brennan@xmos.com>
2017-10-03 10:42:28 +0100

    Simplify stdoutput

--- scripts/avs-pyauth.sh
+++ scripts/avs-pyauth.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 cd $HOME/BUILD
 sudo apt-get install -y python-flask python-requests | sed "s/^/[py packages] /"
-python AuthServer/AuthServer.py &
-ServerPID=$! &
-chromium-browser http://localhost:3000
-wait $ServerPID
+python AuthServer/AuthServer.py > /dev/null &
+chromium-browser http://localhost:3000 > /dev/null 2>&1
+wait
 cp $HOME/BUILD/Integration/AlexaClientSDKConfig.json $SCRIPTS_DIR/
