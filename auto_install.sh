4acda8c55246bb85ca264886bab6e359e8b5af53
Daniel Follent <danielf@xmos.com>
2018-03-27 10:48:55 +0100

    Restructured repo

--- auto_install.sh
+++ auto_install.sh
@@ -1,4 +1,23 @@
-SCRIPTS_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/scripts
+#!/usr/bin/env bash
 
-# Execute (rather than source) the main
-$SCRIPTS_DIR/avs-main.sh
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd . > /dev/null
+cd $SCRIPT_DIR
+
+git clone git://github.com/xmos/vocalfusion-rpi-setup.git
+git clone git://github.com/xmos/avs-sdk-setup.git
+
+RPI_SETUP_DIR=$SCRIPT_DIR/vocalfusion-rpi-setup
+AVS_SETUP_DIR=$SCRIPT_DIR/avs-sdk-setup
+
+# Execute (rather than source) the setup scripts
+$RPI_SETUP_DIR/setup.sh
+
+echo "Installing Amazon AVS SDK..."
+
+$AVS_SETUP_DIR/setup.sh
+
+echo "Type 'sudo reboot' below to reboot the Raspberry Pi and complete the audio setup."
+
+popd > /dev/null
